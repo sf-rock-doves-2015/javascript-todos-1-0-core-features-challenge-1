ebc179027a264703b6febdcfa93d0f9648823d41
Jesse C & Lawrence Yu <pair+jessc.lawyu89@devbootcamp.com>
2015-04-13 11:06:47 -0700

    complete todo list

--- todo_list.js
+++ todo_list.js
@@ -1,11 +1,32 @@
 var createTodoList = function() {
-  var todoList = {};
-
+  var todoList = {
+    list_array: [],
+    add: function(item){
+      this.list_array.push({description: item, completed: false});
+    },
+    list: function(){
+      return this.list_array
+    },
+    indexOf: function(item){
+      for (var i=0; i<this.list_array.length; i++){
+        if (this.list_array[i].description === item){
+          return i
+        };
+      };
+    },
+    get: function(index) {
+      return this.list_array[index]
+    },
+    remove: function(index){
+      delete this.list_array[index]
+    },
+    complete: function(index) {
+      this.list_array[index].completed = true
+    }
   // your code here
-
+  };
   return todoList;
 };
-
 
 
 
@@ -31,20 +52,20 @@ groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
