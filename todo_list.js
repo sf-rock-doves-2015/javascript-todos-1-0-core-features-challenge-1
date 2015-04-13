345aaf9b3a7e3a40b8a9d29d7da54331922a3f38
Emily G & Silas Kwong <pair+emilygern.skwongg@devbootcamp.com>
2015-04-13 12:05:18 -0700

    add changes to task and list functions

--- todo_list.js
+++ todo_list.js
@@ -1,40 +1,38 @@
 var createTodoList = function() {
   var todoList = {};
-  var todoItems = [];
+
+  todoList.tasks = [];
 
   todoList.add = function(item) {
-    todoItems.push({description: item, completed: false});
+    this.tasks.push({description: item, completed: false});
   };
 
-  todoList.tasks = todoItems;
   // this.tasks = function() {
   //   return todoList;
   // };
 
   todoList.list = function() {
-    for (i=0; i< todoItems.length; i++) {
-      console.log(todoItems[i]);
-    };
+    return this.tasks;
   };
 
   todoList.indexOf = function(task) {
-    for (i=0; i<todoItems.length; i++) {
-      if (todoItems[i].description === task) {
+    for (i=0; i<this.tasks.length; i++) {
+      if (this.tasks[i].description === task) {
         return i;
       };
     };
   };
 
   todoList.remove = function(index) {
-    todoItems.splice(index, 1);
+    this.tasks.splice(index, 1);
   };
 
   todoList.get = function(index) {
-    return todoItems[index];
+    return this.tasks[index];
   };
 
   todoList.complete = function(index) {
-    todoItems[index].completed = true;
+    this.tasks[index].completed = true;
   };
 
   return todoList;
@@ -50,14 +48,15 @@ var createTodoList = function() {
 
 // Release 1
 
-// var groceryList = createTodoList();
-// groceryList.add('bread');
-// groceryList.add('cheese');
-// groceryList.add('milk');
-// groceryList.list(); //-> ['bread', 'cheese', 'milk']
-// groceryList.indexOf('cheese'); //-> 1
-// groceryList.remove(1);
-// groceryList.list(); //-> ['bread', 'milk']
+var groceryList = createTodoList();
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+groceryList.tasks; //-> ['bread', 'cheese', 'milk']
+groceryList.list(); //-> ['bread', 'cheese', 'milk']
+groceryList.indexOf('cheese'); //-> 1
+groceryList.remove(1);
+groceryList.list(); //-> ['bread', 'milk']
 
 // // release 2
 var groceryList = createTodoList();
