92d0a4573d17157fdf67442d303037750d22bc8a
gwmartin <ginny.w.martin@gmail.com>
2015-04-13 15:15:44 -0700

    complete

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,43 @@
 var createTodoList = function() {
-  var todoList = {};
+  var todoList = {
+    tasks: [],
 
+    add: function(item) {
+      this.tasks.push({
+        description: item,
+        completed: false,
+      });
+    },
+    list: function(){
+      return this.tasks;
+    },
+
+    indexOf: function(item){
+        for(var i=0; i < this.tasks.length; i++){
+          if (this.tasks[i].description === item)
+            return i;
+        }
+      },
+
+    remove: function(index){
+      this.tasks.splice(index,1);
+    },
+
+    get: function(index){
+      return this.tasks[index]
+     },
+
+    complete: function(index){
+      this.tasks[index].completed = true
+      },
+    };
   // your code here
 
   return todoList;
 };
+
+ginnylist = createTodoList()
+
 
 
 
@@ -31,20 +64,20 @@ groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+  // {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
