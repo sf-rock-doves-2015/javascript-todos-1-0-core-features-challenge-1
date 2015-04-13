8b6b9cf2862519c7a7debf69162d563951743952
Michael Avtonomoff & Lynne Tye <pair+avtomate.lynnetye@devbootcamp.com>
2015-04-13 11:00:52 -0700

    complete challenge

--- todo_list.js
+++ todo_list.js
@@ -1,29 +1,68 @@
+// RELEASE 1 SOLUTION
+// var createTodoList = function() {
+//   var todoList = {
+//     items: [],
+//     add: function(item){
+//       this.items.push(item);
+//     },
+//     list: function(){
+//       return this.items;
+//     },
+//     indexOf: function(item){
+//       return this.items.indexOf(item);
+//     },
+//     remove: function(index){
+//       this.items.splice(index,1);
+//     }
+//   };
+//   // your code here
+//   return todoList;
+// };
+
+
+// RELEASE 2 SOLUTION
 var createTodoList = function() {
-  var todoList = {};
-
-  // your code here
-
+  var todoList = {
+    items: [],
+    add: function(item){
+      this.items.push({description: item, completed: false});
+    },
+    list: function(){
+      return this.items;
+    },
+    indexOf: function(item){
+      items = this.items
+      for(var i = 0; i < this.items.length; i++){
+        if(items[i]["description"] === item) {
+          return i;
+        }
+      }
+    },
+    get: function(index){
+      return this.items[index];
+    },
+    complete: function(index){
+      this.items[index].completed = true
+    },
+    remove: function(index){
+      this.items.splice(index,1);
+    }
+  };
   return todoList;
 };
 
-
-
-
-
-
-// Driver code
-
-
+// DRIVER CODE:
 // Release 1
 
-var groceryList = createTodoList();
-groceryList.add('bread');
-groceryList.add('cheese');
-groceryList.add('milk');
-groceryList.list(); //-> ['bread', 'cheese', 'milk']
-groceryList.indexOf('cheese'); //-> 1
-groceryList.remove(1);
-groceryList.list(); //-> ['bread', 'milk']
+// var groceryList = createTodoList();
+// groceryList.add('bread');
+// groceryList.add('cheese');
+// groceryList.add('milk');
+// groceryList.list(); //-> ['bread', 'cheese', 'milk']
+// groceryList.indexOf('cheese'); //-> 1
+// groceryList.remove(1);
+// groceryList.list(); //-> ['bread', 'milk']
+
 
 // release 2
 var groceryList = createTodoList();
@@ -31,20 +70,21 @@ groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
+
