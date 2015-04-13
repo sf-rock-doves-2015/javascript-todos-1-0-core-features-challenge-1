dd92ca87acfd52b0fa84b4c9a4d0b23b48ca428c
Sara Gilford <sara.gilford@gmail.com>
2015-04-13 11:42:02 -0700

    complete challenge

--- todo_list.js
+++ todo_list.js
@@ -1,8 +1,27 @@
 var createTodoList = function() {
-  var todoList = {};
-
-  // your code here
-
+  var todoList = {
+    tasks: [],
+    add: function(item) {
+      this.tasks.push({description: item, completed: false});
+    },
+    list: function() {
+      for (var i = 0; i < this.tasks.length; i++) {
+        console.log(this.tasks[i]);
+      }
+    },
+    remove: function(index) {
+      this.tasks.slice(index,1);
+    },
+    indexOf: function(item) {
+      this.tasks.indexOf(item);
+    },
+    get: function(index) {
+      this.tasks[index];
+    },
+    complete: function(index) {
+      this.tasks[index].completed = true;
+    }
+  };
   return todoList;
 };
 
@@ -31,20 +50,20 @@ groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
