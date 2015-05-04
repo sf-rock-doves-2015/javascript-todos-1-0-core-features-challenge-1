b6b02200ef9af61c0cd1515c0a31258114d84c2b
Hugh Sutton-Gee & Jerome Chenette <pair+hsuttong.jeromechenette@devbootcamp.com>
2015-05-04 12:17:39 -0700

    Add completed releases 1 & 2

--- todo_list.js
+++ todo_list.js
@@ -1,50 +1,73 @@
 var createTodoList = function() {
   var todoList = {};
+  todoList.cart = [];
+  todoList.add = function(item) {
 
-  // your code here
+    this.cart.push( {description: item, completed: false} );
+  };
+  todoList.list = function(item) {
+    for(var i = 0; i < this.cart.length; i++) {
+      console.log(this.cart[i]);
+    }
+  };
+  todoList.indexOf = function(item) {
+    for(var i = 0; i < this.cart.length; i++) {
+      if (this.cart[i].description === item) {
+        console.log(i);
+        return i;
+
+      }
+    }
+  };
+  todoList.remove = function(indexOf) {
+    this.cart.splice(indexOf, 1)
+  };
+  todoList.get = function(index) {
+    console.log(this.cart[index]);
+    return this.cart[index];
+  };
+  todoList.complete = function(index) {
+      this.cart[index].completed = true;
+  };
 
   return todoList;
 };
 
 
 
-
-
-
 // Driver code
-
-
 // Release 1
 
 var groceryList = createTodoList();
-groceryList.add('bread');
-groceryList.add('cheese');
-groceryList.add('milk');
-groceryList.list(); //-> ['bread', 'cheese', 'milk']
-groceryList.indexOf('cheese'); //-> 1
-groceryList.remove(1);
-groceryList.list(); //-> ['bread', 'milk']
+
+// groceryList.add('bread');
+// groceryList.add('cheese');
+// groceryList.add('milk');
+// groceryList.list(); //-> ['bread', 'cheese', 'milk']
+// groceryList.indexOf('cheese'); //-> 1
+// groceryList.remove(1);
+// groceryList.list(); //-> ['bread', 'milk']
 
 // release 2
 var groceryList = createTodoList();
 groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
-groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+//groceryList.list(); //-> [
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
-groceryList.indexOf('cheese'); //-> 1
-groceryList.get(1); //-> {description: 'cheese', completed: false}
+//groceryList.indexOf('cheese'); //-> 1
+//groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
-groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+//groceryList.list(); //-> [
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
