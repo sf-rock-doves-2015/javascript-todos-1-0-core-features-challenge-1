01ccf0f26342643f7bc0f9eec285faf73ce01dfd
Edwin Unger & Guilherme Sá <pair+edwinunger.guilsa@devbootcamp.com>
2015-05-04 12:10:03 -0700

    19   Star 0  Fork 11sf-pocket-gophers-2015/javascript-todos-1-0-core-features-challenge-1

--- todo_list.js
+++ todo_list.js
@@ -1,7 +1,43 @@
 var createTodoList = function() {
-  var todoList = {};
+  var items = [];
+  var todoList = {
 
-  // your code here
+    add: function(item) {
+      items.push({description: item, completed: false});
+    },
+
+    list: function() {
+      for (i = 0; i < items.length; i++) {
+        console.log(items[i]);
+      };
+    },
+
+    indexOf: function(description) {
+      for (i = 0; i < items.length; i++) {
+        if (items[i].description === description) {
+          console.log(i);
+        }
+      };
+    },
+
+    remove: function(item_index) {
+      items.splice(item_index, 1);
+      return items;
+    },
+
+    get: function(item_index) {
+      items[item_index];
+    },
+
+    complete: function(item_index) {
+      for (i = 0; i < items.length; i++) {
+        if (i === item_index) {
+          console.log(items[i].completed = true);
+        }
+      };
+    }
+
+  };
 
   return todoList;
 };
@@ -31,20 +67,20 @@ groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
